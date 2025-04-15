e60014559b655e5958a2a7e0b6ed47b0990f9d08
Shaughn <Shaughn@MacBook-Air-4.local>
2025-04-15 15:14:57 +0100

    Initial styling for project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,14 +6,22 @@ export default function Project({projectData}:{projectData:project}){
 //might be nice to have an expanded view that shows up but maybe thats better handles in the era component
 
 return(
-    <div>
-        <a href={projectData.link}>
-<h3>
-    {projectData.title}
-</h3>
-<img src={projectData.imageUrl} alt={projectData.altText} className="max-w-50"/>
-<p>{projectData.description}</p>
-</a>
+    <div className="w-2/5">
+        <a href={projectData.link} className="block">
+            <h3 className="mb-2 font-semibold text-lg">
+                {projectData.title}
+            </h3>
+            <div className="relative group">
+                <img 
+                    src={projectData.imageUrl} 
+                    alt={projectData.altText} 
+                    className="w-full h-auto object-cover" 
+                />
+                <div className="absolute inset-0 bg-gray-900/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center p-4">
+                    <p className="text-white text-center">{projectData.description}</p>
+                </div>
+            </div>
+        </a>
     </div>
 )
 
